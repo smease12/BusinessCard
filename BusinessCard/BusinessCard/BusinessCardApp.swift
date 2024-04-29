2d928e6f41aee75ba430bce63836dd9b0e2481d2
smease12 <scmease93@gmail.com>
2024-04-28 21:28:43 -0400

    Initial commit

---
+++ BusinessCard/BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Serena  on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
