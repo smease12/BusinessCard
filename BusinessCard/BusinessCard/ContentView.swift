2628ad5ac6c404095aa5b272e65dfc8fc2408076
smease12 <scmease93@gmail.com>
2024-04-29 21:38:57 -0400

    Set background color to green

--- BusinessCard/BusinessCard/ContentView.swift
+++ BusinessCard/BusinessCard/ContentView.swift
@@ -9,13 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.green).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Text("Hello World!")
         }
-        .padding()
     }
 }
 
