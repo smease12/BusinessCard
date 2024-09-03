5b747f8525f82ce75c8a9a66e3cd90708b4ef50d
smease12 <scmease93@gmail.com>
2024-09-02 20:27:05 -0400

    Update background color with custom color from flat ui colors

--- BusinessCard/BusinessCard/ContentView.swift
+++ BusinessCard/BusinessCard/ContentView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            Color(.green).edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
             Text("Hello World!")
         }
     }
